1de10afcc924fcf970608bd9a0ab05be149aa4b0
Raymond Dodge <git@rayrobdod.name>
2013-12-15 00:53:50 -0500

    Giving some items explicit licenses. Moving MyMenuBar.scala to MyMenuItem.scala, due to the thing's name

--- src/main/java/com/rayrobdod/swing/GridBagConstraintsFactory.scala
+++ src/main/java/com/rayrobdod/swing/GridBagConstraintsFactory.scala
@@ -1,30 +1,17 @@
 /*
-	Copyright (c) 2012-2013, Raymond Dodge
-	All rights reserved.
+	Honestly, this being independently invented is a thousand times
+	more likely than someone finding this. On that note:
 	
-	Redistribution and use in source and binary forms, with or without
-	modification, are permitted provided that the following conditions are met:
-		* Redistributions of source code must retain the above copyright
-		  notice, this list of conditions and the following disclaimer.
-		* Redistributions in binary form must reproduce the above copyright
-		  notice, this list of conditions and the following disclaimer in the
-		  documentation and/or other materials provided with the distribution.
-		* Neither the name "Image Manipulator" nor the names of its contributors
-		  may be used to endorse or promote products derived from this software
-		  without specific prior written permission.
+	Copyright (C) 2013 Raymond Dodge
 	
-	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-	ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-	WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-	DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
-	DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-	(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-	LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-	ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-	(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-	SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*/
+	        DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE 
+                    Version 2, December 2004 
 
+            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE 
+   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION 
+
+  0. You just DO WHAT THE FUCK YOU WANT TO.
+*/
 package com.rayrobdod.swing
 
 import java.awt.{GridBagConstraints, Insets}
--- src/main/java/com/rayrobdod/swing/NullReplaceListCellRenderer.java
+++ src/main/java/com/rayrobdod/swing/NullReplaceListCellRenderer.java
@@ -1,3 +1,17 @@
+/*
+	Honestly, this being independently invented is a thousand times
+	more likely than someone finding this. On that note:
+	
+	Copyright (C) 2013 Raymond Dodge
+	
+	        DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE 
+                    Version 2, December 2004 
+
+            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE 
+   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION 
+
+  0. You just DO WHAT THE FUCK YOU WANT TO.
+*/
 package com.rayrobdod.swing;
 
 import javax.swing.ListCellRenderer;
